cfb06f816c38afdf68c4b122d0ed4c504f2f12e7
abhijit <abhijityenare955@gmail.com>
2026-08-16 02:41:10 +0530

    Fix Puppeteer PDF generation for Render

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,8 +1,7 @@
-import { GoogleGenAI } from "@google/genai";
-import { z } from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
-import puppeteer from "puppeteer";
-
+const { GoogleGenAI } = require("@google/genai")
+const { z } = require("zod")
+const { zodToJsonSchema } = require("zod-to-json-schema")
+const puppeteer = require('puppeteer')
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
 })
