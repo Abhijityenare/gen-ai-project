3b5da038d5e23022ec92bdc163aa32da587dbb34
abhijit <abhijityenare955@gmail.com>
2026-08-16 00:54:49 +0530

    frontend integration comple

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require('path')
+
 
 const app = express()
 
 app.use(express.json())
+app.use(express.static("./public"))
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
@@ -20,6 +23,10 @@ const interviewRouter = require("./routes/interview.routes")
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"))
+})
 
 
 module.exports = app
